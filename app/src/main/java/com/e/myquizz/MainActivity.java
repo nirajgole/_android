20080a6376360de140b3b48d726d8faaf7badec1
nirajgole <nirajgole@protonmail.ch>
2020-05-14 20:45:35 +0530

    login and sign_up

---
+++ app/src/main/java/com/e/myquizz/MainActivity.java
@@ -0,0 +1,47 @@
+package com.e.myquizz;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+import com.e.myquizz.fragments.Topics;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.ui.AppBarConfiguration;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.topics, R.id.friends, R.id.message,R.id.achievements,R.id.settings,R.id.logout)
+                .setDrawerLayout(drawer)
+                .build();
+
+        getSupportFragmentManager().beginTransaction().add(R.id.frameLayout,new Topics()).commit();
+        //my code
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+}
